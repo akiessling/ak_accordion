56255604824ecfa2fca0b35620d56038e781fd11
Andreas Kießling <andreas.kiessling@web.de>
2012-11-28 00:29:23 +0100

    "New Content Element Wizard" was missing setting for CType defValues

--- Configuration/TsConfig/PageTs/wizard.ts
+++ Configuration/TsConfig/PageTs/wizard.ts
@@ -2,6 +2,9 @@ mod.wizards.newContentElement.wizardItems.common.elements.ak_accordion {
 	icon = ../typo3conf/ext/ak_accordion/Resources/Public/Icons/Wizard/plugin_accordion.png
 	title = LLL:EXT:ak_accordion/Resources/Private/Language/locallang_wizard.xlf:plugin_accordion
 	description = LLL:EXT:ak_accordion/Resources/Private/Language/locallang_wizard.xlf:plugin_accordion_description
+	tt_content_defValues {
+		CType = akaccordion_accordion
+	}
 }
 
 mod.wizards.newContentElement.wizardItems.common.show := addToList(ak_accordion)
