ee316511ade5c00ace8450f5163bf931cb76f2c2
Andreas Kießling <andreas.kiessling@web.de>
2013-06-20 20:14:26 +0200

    replaced call to deprecated t3lib_extMgm

--- ext_localconf.php
+++ ext_localconf.php
@@ -12,6 +12,6 @@ if (!defined('TYPO3_MODE')) {
 	\TYPO3\CMS\Extbase\Utility\ExtensionUtility::PLUGIN_TYPE_CONTENT_ELEMENT
 );
 
-t3lib_extMgm::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:' . $_EXTKEY . '/Configuration/TsConfig/PageTs/wizard.ts">');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:' . $_EXTKEY . '/Configuration/TsConfig/PageTs/wizard.ts">');
 
 ?>
